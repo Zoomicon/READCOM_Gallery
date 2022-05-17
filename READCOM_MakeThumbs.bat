c804b10d839e18c3c2685d86f3cc2c975d30f70a
birbilis@zoomicon.com <George>
2022-05-17 04:56:10 +0300

    Added thumbnails generator (works with "READCOM_App -thumb filename" syntax of upcoming version 0.5.13). Creates .thumbs subfolders

---
+++ READCOM_MakeThumbs.bat
@@ -0,0 +1,37 @@
+@echo off
+
+echo Syntax: READCOM_MakeThumbs [folder]
+
+::--------------------------------
+
+if not exist %~dp0READCOM_App.exe echo %~dp0READCOM_App.exe not found & pause & exit 1
+
+SET folder=%1
+if "%folder%"=="" SET folder=.
+
+call :processFolder "%folder%"
+
+pause
+exit 0
+
+::--------------------------------
+
+:processFolder
+if "%~n1"==".thumbs" exit /B
+echo Processing folder %1
+
+for /D %%d in (%1\*.*) do call :processFolder "%%d"
+for %%f in (%1\*.readcom) do call :processFile "%%f"
+
+exit /B
+
+::--------------------------------
+
+:processFile
+echo Saving thumb for %1
+%~dp0READCOM_App -thumb %1
+
+if not exist "%~p1.thumbs"\ echo Creating folder "%~p1.thumbs" & md "%~p1.thumbs"
+move %1.png "%~p1.thumbs\" >NUL
+
+exit /B
