cc2179ba22a0eb5f5fe37dbb29686c1f578824f6
birbilis@zoomicon.com <George>
2022-07-07 17:57:58 +0300

    Updated thumbs generator and regenerated thumbs

--- READCOM_MakeThumbs.bat
+++ READCOM_MakeThumbs.bat
@@ -1,11 +1,11 @@
 @echo off
 
-echo Syntax: READCOM_MakeThumbs [file|folder]
+echo Syntax: READCOM_MakeThumbs [file^|folder]
 echo Processes Gallery subfolder by default
 
 ::--------------------------------
 
-if not exist %~dp0READCOM_App.exe echo %~dp0READCOM_App.exe (need at least version 0.5.13) not found & pause & exit 1
+if not exist "%~dp0READCOM_App.exe" echo %~dp0READCOM_App.exe (need at least version 0.5.13) not found & pause & exit 1
 
 SET param=%1
 if "%param%"=="" SET param=Gallery
@@ -24,20 +24,27 @@ exit 0
 :processFolder
 if "%~nx1"==".thumbs" exit /B
 if "%~nx1"==".git" exit /B
+
+echo:
 echo Processing folder %1
 
-for /D %%d in (%1\*.*) do call :processFolder "%%d"
+:: Process files
+if exist "%~df1\.thumbs" echo Clearing subfolder ".thumbs" & (del "%~df1\.thumbs\*.*" /Q >NUL)
 for %%f in (%1\*.readcom) do call :processFile "%%f"
 
-exit /B
+:: Process subfolders
+for /D %%d in (%1\*.*) do call :processFolder "%%d"
+
+exit /B 
 
 ::--------------------------------
 
 :processFile
-echo Saving thumb for %1
-%~dp0READCOM_App -thumb %1
+:: Note that %~dp1 always ends with a \
+if not exist "%~dp1.thumbs" echo Creating subfolder ".thumbs" & md "%~dp1.thumbs"
 
-if not exist "%~p1.thumbs"\ echo Creating folder "%~p1.thumbs" & md "%~p1.thumbs"
-move %1.png "%~p1.thumbs\" >NUL
+echo Saving thumb for %1
+%~dp0READCOM_App -thumb "%~f1"
+move %1.png "%~dp1.thumbs\" >NUL
 
 exit /B
